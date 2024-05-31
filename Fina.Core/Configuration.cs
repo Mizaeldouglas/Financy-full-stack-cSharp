918731c869f10e4dbbe118344b170ced6db4f540
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-05-31 16:04:23 -0300

    category do frontend finalizado

--- Fina.Core/Configuration.cs
+++ Fina.Core/Configuration.cs
@@ -6,6 +6,6 @@ public class Configuration
     public const int DefaultPageNumber = 1;
     public const int DefaultPageSize = 25;
     
-    public static string BackendUrl { get; set; } = "http://localhost:5250";
-    public static string FrontendUrl { get; set; } = "http://localhost:5200";
+    public static string BackendUrl { get; set; } = "http://localhost:5199";
+    public static string FrontendUrl { get; set; } = "http://localhost:5236";
 }
